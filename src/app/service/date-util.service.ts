c6e0afc6afc90519b1df920d59671241b140fe58
zhuy-nguyenz <graio911@gmail.com>
2021-09-23 10:26:38 +0700

    1

---
+++ src/app/service/date-util.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {addMonths, addYears, differenceInDays, differenceInMonths, differenceInYears} from 'date-fns'
+@Injectable({
+  providedIn: 'root'
+})
+export class DateUtilService {
+
+  constructor() { }
+
+  getDiffToNow(diff:string|number|Date):string{
+    const result :string[] = [];
+    const now = new Date();
+    diff = new Date(diff);
+    const years = differenceInYears(now,diff);
+    if(years >0){
+      result.push(`${years} years`);
+      diff = addYears(diff, years);
+    }
+    const month = differenceInMonths(now,diff);
+    if(month > 0){
+      result.push(`${month} months`);
+      diff = addMonths(diff, month);
+    }
+
+    const days = differenceInDays(now,diff);
+    if (days > 0 ){
+      result.push(`${days} days`)
+    }
+    return result.join(' ')
+  }
+}
