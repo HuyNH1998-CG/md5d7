23fc85764013fb6183bca41fe6ff39dca2cd1abb
zhuy-nguyenz <graio911@gmail.com>
2021-09-23 11:14:59 +0700

    2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {TimelinesComponent} from "./timelines/timelines.component";
+import {YoutubePlaylistComponent} from "./youtube-playlist/youtube-playlist.component";
+import {YoutubePlayerComponent} from "./youtube-player/youtube-player.component";
 
 const routes: Routes = [{
-  path:'timelines',
+  path: 'timelines',
   component: TimelinesComponent
+}, {
+  path: 'youtube',
+  component: YoutubePlaylistComponent,
+  children:[{
+    path:':id',
+    component: YoutubePlayerComponent
+  }]
 }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
